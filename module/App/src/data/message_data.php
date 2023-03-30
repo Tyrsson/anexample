900d504fba2d13c8d36c7b5a76b56dfd390418bb
Joey Smith <jsmith@webinertia.net>
2023-03-29 21:29:28 -0500

    Updating data appliance Signed-off-by: Joey Smith <jsmith@webinertia.net>
    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- module/App/src/data/message_data.php
+++ module/App/src/data/message_data.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 use App\Model\MessageInterface;
-
+// Gotta love arrays =)
 return [
     MessageInterface::MESSAGE_TYPE_EMAIL => [
         [
@@ -19,6 +19,40 @@ return [
                     'addresses'  => [
                         'sender'    => 'ana@webinertia.net',
                         'recipient' => 'jsmith@webinertia.net',
+                    ],
+                    'timestamps' => [ // What could the following values possibly tell us? These are all suedo values...
+                        'sent'                  => true,
+                        'received'              => true,
+                        'sender_notified'       => false,
+                        'recipient_notfied'     => true,
+                        'read_receipt_received' => false,
+                    ],
+                    MessageInterface::MESSAGE_TYPE_NOTIFCATION => [
+                        [
+                            'type'     => MessageInterface::NOTIFICATION_TYPE_SMS,
+                            'priority' => 1,
+                        ],
+                        [
+                            'type'     => MessageInterface::NOTIFICATION_TYPE_PUSH,
+                            'priority' => 2,
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        [
+            'sender' => [
+                'firstName' => 'Joey',
+                'lastName'  => 'Smith',
+            ],
+            'message' => [
+                'subject'   => 'It\'s not broken :) that\'s a feature!!!', // why is the ' preceeded with a \ ?
+                'body'      => 'Open a terminal and cd /path/to/parent | sudo rm -r projectFolder \n Problem fixed =)', // do not!!! run these commands :P
+                'meta_data' => [
+                    'type' => MessageInterface::MESSAGE_TYPE_EMAIL,
+                    'addresses'  => [
+                        'sender'    => 'jsmith@webinertia.net',
+                        'recipient' => 'ana@webinertia.net',
                     ],
                     'timestamps' => [
                         'sent'              => null,
