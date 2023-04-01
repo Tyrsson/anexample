8a543a6dbdcb48a94bbe5709e29d431504b8d9bb
Joey Smith <jsmith@webinertia.net>
2023-04-01 12:48:47 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- module/App/src/data/user_data.php
+++ module/App/src/data/user_data.php
@@ -3,12 +3,24 @@
 declare(strict_types=1);
 
 return [
-    'ana' => [
+    'ana'      => [
         'firstName' => 'Ana',
         'lastName'  => 'Geldiashvili',
     ],
-    'joey' => [
+    'joey'     => [
         'firstName' => 'Joey',
         'lastName'  => 'Smith',
+    ],
+    'testUser' => [
+        'firstName' => 'Test',
+        'lastName'  => 'User',
+        'mi'        => 'A',
+        'street'    => '123 Echo drive',
+        'city'      => 'Dataville',
+        'zip'       => \dechex(35025), // notice the \ is because we do not have a use function statement at the top of the file
+        'country'   => 'NAN',
+        'planet'    => 'Cybertron',
+        'galaxy'    => 'One far away',
+
     ],
 ];
--- public/index.php
+++ public/index.php
@@ -78,3 +78,21 @@ Debug::dump($joey, 'Dumping $joey', true);
 // this was a test to verify that the correct line was found in the stack trace ;)
 $joey->readMessage();
 
+$var = ['key' => 'value'];
+$anotherVar = ['anotherKey' => 'anotherValue'];
+$merged = [];
+$merged += $var + $anotherVar;
+Debug::dump($merged);
+Debug::dump(array_merge($var, $anotherVar));
+
+/**
+ * So what would a $user->fetch* method look like for our current User model/service?
+ * what is the purpose of this method? Why do we need it? How can we refactor User to take advantage of one to improve
+ * encapsulation and to make accessing the user data simpler and allow any type of data structure to used to provide said user data?
+ * Currently we have user data supplied by a associative array, but what if it was returned by a MySQL query? Odds are it would still
+ * end up at this point as an array of results, usually one array per user, so whats the difference between what we will end up having
+ * and what we have now? As long as we plan correctly not a single line of code will have to change outside of the User class
+ * So lets take a look at what we will need.
+ *
+ * */
+
