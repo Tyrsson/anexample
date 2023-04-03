15a7f238d9010bd9a78a98f9d84818bb2746b934
Joey Smith <jsmith@webinertia.net>
2023-04-03 12:58:17 -0500

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- module/App/src/Message.php
+++ module/App/src/Message.php
@@ -8,5 +8,13 @@ use App\Model\AbstractMessage;
 
 final class Message extends AbstractMessage
 {
+    /** This fixes the error */
+    protected function readMessage() { }
+
+    protected function sendMessage() { }
+
+    protected function notifyMessageRead() { }
+
+    protected function notifyMessageSent() { }
 
 }
